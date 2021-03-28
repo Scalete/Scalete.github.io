95ab45ad828cdcb12721a97ea13de1160473d869
Max <maximpalaguta@gmail.com>
2021-03-28 18:20:16 +0300

    reduced the size of the main bg

--- js/script.js
+++ js/script.js
@@ -1,54 +1,56 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close'),
-    overlay = document.querySelector('.menu__overlay'),
-    menuList = document.querySelectorAll('.menu__link');
+$(document).ready(function () {
+    const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close'),
+        overlay = document.querySelector('.menu__overlay'),
+        menuList = document.querySelectorAll('.menu__link');
 
-//open menu
+    //open menu
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-//close menu
-
-function closeMenu(closeElem) {
-    closeElem.addEventListener('click', () => {
-        menu.classList.remove('active');
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
     });
-}
 
-closeMenu(closeElem);
-closeMenu(overlay);
+    //close menu
 
-menuList.forEach(item => {
-    closeMenu(item);
-});
-
-//auto progress bar
-
-const counters = document.querySelectorAll('.work__bar-title span'),
-    lines = document.querySelectorAll('.work__bar-line span');
-
-counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
-
-//change sidepanel color
-
-$(window).scroll(function () {
-    var top = $(this).scrollTop();
-    if (top >= $('.promo').height() / 2) {
-        changeSidepanelColor('black');
-    } else {
-        changeSidepanelColor('white');
+    function closeMenu(closeElem) {
+        closeElem.addEventListener('click', () => {
+            menu.classList.remove('active');
+        });
     }
+
+    closeMenu(closeElem);
+    closeMenu(overlay);
+
+    menuList.forEach(item => {
+        closeMenu(item);
+    });
+
+    //auto progress bar
+
+    const counters = document.querySelectorAll('.work__bar-title span'),
+        lines = document.querySelectorAll('.work__bar-line span');
+
+    counters.forEach((item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
+
+    //change sidepanel color
+
+    $(window).scroll(function () {
+        var top = $(this).scrollTop();
+        if (top >= $('.promo').height() / 2) {
+            changeSidepanelColor('black');
+        } else {
+            changeSidepanelColor('white');
+        }
+    });
+
+    function changeSidepanelColor(color) {
+        $(".sidepanel__link svg path").css('fill', color);
+        $(".sidepanel__divider").css('background-color', color);
+        $(".sidepanel__text").css('color', color);
+    }
+
+    new WOW().init();
 });
-
-function changeSidepanelColor(color) {
-    $(".sidepanel__link svg path").css('fill', color);
-    $(".sidepanel__divider").css('background-color', color);
-    $(".sidepanel__text").css('color', color);
-}
-
-new WOW().init();
